87d15d5abe0279d958bc8e16b57fc165564c98bf
anatolk <anatolk@umich.edu>
2024-09-04 20:09:09 -0400

    completed inertia script

--- Assignment_1/inertia/inertia.cpp
+++ Assignment_1/inertia/inertia.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <fstream>
 
+double getCuboidInertia(const double &mass, const double &width, const double &length)
+{
+    double I = 0.0;
+    I = static_cast<double>(1 / 12.0) * mass * (pow(width, 2) + pow(length, 2));
+    return I;
+}
+
 int main()
 {
     double mass, w, h, d;
@@ -10,12 +17,15 @@ int main()
     ifs >> w;
     ifs >> h;
     ifs >> d;
-    // you'll need to store the outputs in variables Ih, Iw, and Id.
-    // --- Your code here
 
+    double Ih;
+    double Iw;
+    double Id;
 
+    Ih = getCuboidInertia(mass, w, d);
+    Iw = getCuboidInertia(mass, d, h);
+    Id = getCuboidInertia(mass, w, h);
 
-    // ---
     std::cout << "<ixx>" << Ih << "</ixx>\n";
     std::cout << "<iyy>" << Iw << "</iyy>\n";
     std::cout << "<izz>" << Id << "</izz>\n";
