fa80e62450cb5deb17387d606de3ca2492c5c800
anatolk <anatolk@umich.edu>
2024-09-06 15:04:22 -0400

    Problem 5, mi_to_km. Completed miles to km conversion script mi_to_km.cpp

--- Assignment_1/mi_to_km/mi_to_km.cpp
+++ Assignment_1/mi_to_km/mi_to_km.cpp
@@ -1,5 +1,19 @@
-// --- Your code here
+#include <iostream>
+#include <iomanip>
 
+double miToKm(const double &miles)
+{
+    return miles * 1.60934;
+}
 
+int main()
+{
+    double miles, km;
 
-// ---
+    std::cin >> miles;
+    km = miToKm(miles);
+
+    std::cout << std::setprecision(3);
+    std::cout << km << std::endl;
+    return 0;
+}
