84444e2d8bb9b1b20ff49d52b6a1b7c671b5bae8
anatolk <anatolk@umich.edu>
2024-09-06 14:35:31 -0400

    Problem 4 volume.cpp. Completed cone volume from r and h volume.cpp

--- Assignment_1/volume/volume.cpp
+++ Assignment_1/volume/volume.cpp
@@ -2,18 +2,20 @@
 #include <iomanip>
 #include <iostream>
 
-int main() {
-    // determines volume of a cone (2 args from stdin) 
+double getVolumeOfCone(double radius, double height)
+{
+    return M_PI * radius * radius * height / 3;
+}
+
+int main()
+{
+    // determines volume of a cone (2 args from stdin)
     // again, you should create the variable `volume` for the output.
     double radius, height;
     std::cout << "Enter the radius and height on separate lines\n";
     std::cin >> radius;
     std::cin >> height;
-    // --- Your code here
-
-
-
-    // ---
+    double volume = getVolumeOfCone(radius, height);
     std::cout << std::setprecision(3) << volume << std::endl;
     return 0;
 }
