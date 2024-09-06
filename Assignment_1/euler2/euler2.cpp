d1e5dc46072eb1cc7952bb59a2a2448a53ebcbaa
anatolk <anatolk@umich.edu>
2024-09-06 11:32:44 -0400

    Problem 3, euler 2. Completed sum of even fibonaccies euler2.cpp.

--- Assignment_1/euler2/euler2.cpp
+++ Assignment_1/euler2/euler2.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // you'll need to store the answer in a variable named 'sum'
-    // --- Your code here
+unsigned long fibonacciSumEvens(const unsigned int &firstFibNumber, const unsigned int &secondFibNumber, const unsigned int &maxFibNumber)
+{
+    // trivial case
+    if (firstFibNumber > maxFibNumber)
+    {
+        return 0;
+    }
 
+    long unsigned int previousFibNumber = firstFibNumber;
+    long unsigned int currentFibNumber = secondFibNumber;
+    long unsigned int temp = 0;
 
+    long unsigned int runningSum = previousFibNumber * (previousFibNumber % 2 == 0);
 
-    // ---
+    while (currentFibNumber <= maxFibNumber)
+    {
+        runningSum += currentFibNumber * (currentFibNumber % 2 == 0);
+
+        // update the Fibonacci numbers.
+        temp = currentFibNumber;
+        currentFibNumber += previousFibNumber;
+        previousFibNumber = temp;
+    }
+    return runningSum;
+}
+
+int main()
+{
+
+    unsigned long sum = fibonacciSumEvens(1, 2, 4'000'000);
     std::cout << sum << std::endl;
     return 0;
 }
