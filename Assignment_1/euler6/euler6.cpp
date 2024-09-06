e50977b8df99ba37bb06c8bed40704e6078b3b38
anatolk <anatolk@umich.edu>
2024-09-06 11:52:58 -0400

    Problem 3, euler6. Completed euler6.cpp squaresum sumsquares difference.

--- Assignment_1/euler6/euler6.cpp
+++ Assignment_1/euler6/euler6.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 
-int main() {
-    // --- Your code here
+unsigned long sumOfSquares(const unsigned int &limit)
+{
+    unsigned long sum = 0;
+    for (unsigned int i = 1; i <= limit; i++)
+    {
+        sum += i * i;
+    }
+    return sum;
+}
 
+unsigned long squareOfSum(const unsigned int &limit)
+{
+    unsigned long sum = 0;
+    for (unsigned int i = 1; i <= limit; i++)
+    {
+        sum += i;
+    }
+    return sum * sum;
+}
 
+int main()
+{
+    int limit = 100;
+    unsigned long sumOfSquaresResult = sumOfSquares(limit);
+    unsigned long squareOfSumResult = squareOfSum(limit);
 
-    // ---
+    long long difference = squareOfSumResult - sumOfSquaresResult;
+
     std::cout << difference << std::endl;
     return 0;
 }
