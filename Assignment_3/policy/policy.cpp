f67c820a67385649aa2e4261c79bc6a6595e588e
anatolk <anatolk@umich.edu>
2024-10-05 15:34:36 -0400

    Completed Assignemenet 3 problem 5

--- Assignment_3/policy/policy.cpp
+++ Assignment_3/policy/policy.cpp
@@ -3,79 +3,103 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sstream>
 using namespace std;
 
+using State = vector<int>;  // this defines the type State to be a vector of ints
+using Action = vector<int>; // this defines the type Action to be a vector of ints
+using Policy = map<State, Action>;
 
-using State = vector<int>; //this defines the type State to be a vector of ints
-using Action = vector<int>; //this defines the type Action to be a vector of ints
-
-
-//this function should print out a state to the given stream
-//the ostream could be something like cout, or cerr
-//or it could be a filestream
-void printState(const State& state, ostream & stream){
-    // --- Your code here
-
-
-
-    // ---
+// this function should print out a state to the given stream
+// the ostream could be something like cout, or cerr
+// or it could be a filestream
+void printState(const State &state, ostream &stream)
+{
+    {
+        for (int j = 0; j < state.size(); j++)
+        {
+            stream << state[j] << " ";
+        }
+        stream << endl;
+    }
 }
 
-
-State dynamics(const State& state, const Action& action)
+State dynamics(const State &state, const Action &action)
 {
     State next_state(state.size());
-    for (int j = 0; j < state.size();j++)
+    for (int j = 0; j < state.size(); j++)
     {
         next_state[j] = state[j] + action[j];
     }
     return next_state;
 }
 
+void loadPolicy(std::istream &policyStream, Policy &policy)
+{
+    std::string policyLine;
 
-int main () {
-    
+    // Read the rest of the policy lines
+    while (std::getline(policyStream, policyLine))
+    {
+        std::istringstream policyLineStream(policyLine);
+        State state(2);
+        Action action(2);
+
+        policyLineStream >> state[0] >> state[1] >> action[0] >> action[1];
+
+        policy[state] = action;
+    }
+}
+
+int main()
+{
+
     ifstream policyfile("policy.txt");
     ofstream outputfile("output.txt");
 
     const int StateSize = 2;
-    const int ActionSize = 2;    
+    const int ActionSize = 2;
 
     State key(StateSize);
     Action value(ActionSize);
-    map<State,Action> policy;
+    Policy policy;
 
-    //read the policy file information into std::pair objects,
-    //insert each std::pair into the map
-    // --- Your code here
+    // read the policy file information into std::pair objects,
+    // insert each std::pair into the map
+    loadPolicy(policyfile, policy);
 
-
-
-    // ---
-
-    //optional (but recommended) print out the policy to cout
-    //to make sure that it was read in correctly
-    // --- Your code here
-
-
-
-    // ---
-
-    //read in rollout
-    ifstream rolloutfile ("rollout.txt");
-    State startState(StateSize); //this stores the start stae
-    int numSteps; //this is the number of steps in the rollout
-    rolloutfile>>startState[0];
-    rolloutfile>>startState[1];
-    rolloutfile>>numSteps;
+    // optional (but recommended) print out the policy to cout
+    // to make sure that it was read in correctly
+    for (auto &pair : policy)
+    {
+        printState(pair.first, cout);
+        printState(pair.second, cout);
+        cout << endl;
+    }
+    // read in rollout
+    ifstream rolloutfile("rollout.txt");
+    State startState(StateSize); // this stores the start stae
+    int numSteps;                // this is the number of steps in the rollout
+    rolloutfile >> startState.at(0);
+    rolloutfile >> startState.at(1);
+    rolloutfile >> numSteps;
     rolloutfile.close();
 
-    //Now use the the policy map to do the rollout
-    // --- Your code here
-
-
-
-    // ---
+    // Now use the the policy map to do the rollout
+    State currentState = startState;
+    printState(currentState, outputfile);
+    for (int i = 0; i < numSteps; i++)
+    {
+        if (policy.find(currentState) == policy.end())
+        {
+            cout << "State " << currentState.at(0) << " " << currentState.at(1) << " is not in policy, terminating." << endl;
+            outputfile.close();
+            return 1;
+        }
+        Action action = policy[currentState];
+        currentState = dynamics(currentState, action);
+        printState(currentState, outputfile);
+    }
 
     outputfile.close();
     return 0;
