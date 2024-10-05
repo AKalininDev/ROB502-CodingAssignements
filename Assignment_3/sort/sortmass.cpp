0cabe2f9099a5776d3c12494ec72adf779cfc2ab
anatolk <anatolk@umich.edu>
2024-10-05 13:17:44 -0400

    Completed problem 2

--- Assignment_3/sort/sortmass.cpp
+++ Assignment_3/sort/sortmass.cpp
@@ -1,38 +1,59 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <sstream>
+
 #include <vector>
 #include "sortmass.h"
 
 using namespace std;
 
-int main() {
+ObjMassPair getObjMassPair(const string &objectLine)
+{
+  istringstream iss(objectLine);
+  string name;
+  double mass;
+
+  iss >> name >> mass;
+
+  return ObjMassPair(name, mass);
+}
+
+void readObjects(ifstream &objects, vector<ObjMassPair> &obj_mass)
+{
+  string objectLine;
+
+  while (!objects.eof())
+  {
+    getline(objects, objectLine);
+
+    if (!objectLine.empty())
+    {
+      ObjMassPair object = getObjMassPair(objectLine);
+      obj_mass.emplace_back(object);
+    }
+  }
+}
+
+int main()
+{
   ifstream objects("objects.txt");
   ofstream outfile("output.txt");
   string name;
   double mass;
 
   vector<ObjMassPair> obj_mass;
-  //read the contents of the objects file in to the obj_mass vector
-  // --- Your code here
-
-
+  // read the contents of the objects file in to the obj_mass vector
+  //  --- Your code here
 
   // ---
+  readObjects(objects, obj_mass);
+  sort(obj_mass.begin(), obj_mass.end(), customComparator);
 
-  //use std::sort to sort obj_mass. You will need to define
-  //a function that compares ObjMassPair objects that is of
-  //the form expected by std::sort
-
-  // --- Your code here
-
-
-
-  // ---
-
-  //you will need to properly overload the << operator (in sortmass.h)
-  //to print obj_mass to cout
-  //look at the form expected in the given output.txt
+  // you will need to properly overload the << operator (in sortmass.h)
+  // to print obj_mass to cout
+  // look at the form expected in the given output.txt
   cout << obj_mass;
   outfile << obj_mass;
 }
--- Assignment_3/sort/sortmass.h
+++ Assignment_3/sort/sortmass.h
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <vector>
 
-using ObjMassPair = std::pair<std::string, double>; //this defines the type ObjMassPair to be std::pair<std::string, double>
+using ObjMassPair = std::pair<std::string, double>; // this defines the type ObjMassPair to be std::pair<std::string, double>
 
-//define the operator << here
-//define a function that compares ObjMassPair objects here (for use by std::sort)
-// --- Your code here
+// overloading << operator for ObjMassPair
+std::ostream &operator<<(std::ostream &os, const ObjMassPair &omp)
+{
+    os << omp.first << " " << omp.second;
 
+    return os;
+}
 
+std::ostream &operator<<(std::ostream &os, const std::vector<ObjMassPair> &vec)
+{
+    for (const ObjMassPair &omp : vec)
+    {
+        os << omp << std::endl;
+    }
 
-// ---
+    return os;
+}
+
+// define a function that compares ObjMassPair objects here (for use by std::sort)
+bool operator<(const ObjMassPair &omp1, const ObjMassPair &omp2)
+{
+    return omp1.second < omp2.second;
+}
+
+bool customComparator(const ObjMassPair &omp1, const ObjMassPair &omp2)
+{
+    return omp1.second < omp2.second;
+}
