ebb2786910265a8d081e8f3f0603608c5c3b101f
anatolk <anatolk@umich.edu>
2024-10-27 14:24:37 -0400

    Completed Assignement4, Problem1.

--- Assignment_4/euler4/euler4.cpp
+++ Assignment_4/euler4/euler4.cpp
@@ -3,15 +3,55 @@
 #include <string>
 #include <sstream>
 
+unsigned long reverse(unsigned long n)
+{
+    unsigned long reversed{0};
 
-int main() {
+    while (n > 0)
+    {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return reversed;
+}
+
+bool isPalindrome(unsigned long n)
+{
+    return n == reverse(n);
+}
+
+unsigned long int largestPalindromeProduct(int lower_mult, int upper_mult)
+{
+    int new_lower = lower_mult;
+    int new_upper = upper_mult - 1;
+    long unsigned largestPalindrome = 0;
+
+    if (lower_mult > upper_mult)
+    {
+        return 0;
+    }
+
+    for (int i = upper_mult; i >= lower_mult; i--)
+    {
+        if (isPalindrome(upper_mult * i))
+        {
+            largestPalindrome = upper_mult * i;
+            new_lower = i;
+            break;
+        }
+    }
+
+    return std::max(largestPalindrome, largestPalindromeProduct(new_lower, new_upper));
+}
+
+int main()
+{
     // HINT: consider using std::to_string and std::reverse
-    unsigned long largest_palandrome{0};
+    unsigned long int largestPalindrome{0};
     // --- Your code here
 
-
-
+    largestPalindrome = largestPalindromeProduct(100, 999);
     // ---
-    std::cout << largest_palandrome << std::endl;
+    std::cout << largestPalindrome << std::endl;
     return 0;
 }
