f4360fdf9d787ceb5dffbe8cd204ac9367623225
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-10-28 14:21:18 -0400

    Completed Assignement 4, problem 2.

--- Assignment_4/ransac/ransac.h
+++ Assignment_4/ransac/ransac.h
@@ -18,6 +18,14 @@ struct FitResult
     int n_inliers = -1;
 };
 
+void normalizePlane(Plane &plane, double norm = 1.0)
+{
+    plane.a /= norm;
+    plane.b /= norm;
+    plane.c /= norm;
+    plane.d /= norm;
+}
+
 void pretty_print(std::string const &input_filename, Plane const &plane)
 {
     std::cout << std::setprecision(3) << "--infile " << input_filename << " -a " << plane.a << " -b " << plane.b << " -c " << plane.c << " -d " << plane.d << '\n';
@@ -70,22 +78,7 @@ public:
 
     // by writing `override`, the compiler will check that this function actually overrides something
     // in the base class. Always use this to prevent mistakes in the function signature!
-    FitResult fit(Eigen::MatrixXd const &points) override
-    {
-        // 1. select 3 points from `points` randomly
-        // 2. compute the equation of the plane (HINT: use compute_plane_from_points)
-        // 3. compute the `n_inliers` given that plane equation
-        // (HINT: multiply the points matrix by the normal vector)
-        // --- Your code here
-
-        Eigen::MatrixXd random_points = getRandomPoints(points, 3);
-
-        Plane analytic_plane = compute_plane_from_points(random_points.row(0), random_points.row(1), random_points.row(2));
-        int n_inliers = countInliers(points, analytic_plane);
-        // ---
-
-        return {analytic_plane, n_inliers};
-    }
+    virtual FitResult fit(Eigen::MatrixXd const &points) override;
 
 private:
     Plane compute_plane_from_points(Eigen::Vector3d const &p0, Eigen::Vector3d const &p1, Eigen::Vector3d const &p2);
@@ -97,20 +90,24 @@ public:
     LeastSquaresFitter(int num_points, int n_sample_points) : BaseFitter(num_points), n_sample_points_(n_sample_points) {}
 
     // You should override the `fit` method here
-    // --- Your code here
+    virtual FitResult fit(Eigen::MatrixXd const &points) override;
 
-    // ---
-
+private:
     int const n_sample_points_;
 };
 
 Plane ransac(BaseFitter &fitter, Eigen::MatrixXd const &points)
 {
-    // --- Your code here
+    FitResult best_result;
+    for (int i = 0; i < 10000; i++)
+    {
+        FitResult result = fitter.fit(points);
+        if (result.n_inliers > best_result.n_inliers)
+        {
+            best_result = result;
+        }
+    }
 
-    // ---
-
-    // HINT: the number of inliers should be between 20 and 80 if you did everything correctly
     std::cout << best_result.n_inliers << std::endl;
     return best_result.plane;
 }
@@ -159,5 +156,66 @@ Plane AnalyticFitter::compute_plane_from_points(Eigen::Vector3d const &p0, Eigen
     plane.c = n.z();
     plane.d = -n.dot(p0);
 
+    normalizePlane(plane, plane.a);
+
     return plane;
+}
+
+FitResult AnalyticFitter::fit(Eigen::MatrixXd const &points)
+{
+    // 1. select 3 points from `points` randomly
+    // 2. compute the equation of the plane
+    // 3. compute the `n_inliers` given that plane equation
+    Eigen::MatrixXd random_points = getRandomPoints(points, 3);
+
+    Plane analytic_plane = compute_plane_from_points(
+        random_points.row(0),
+        random_points.row(1),
+        random_points.row(2));
+
+    // normalizing the plane equation
+    double norm = std::sqrt(analytic_plane.a * analytic_plane.a + analytic_plane.b * analytic_plane.b + analytic_plane.c * analytic_plane.c);
+    normalizePlane(analytic_plane, norm);
+
+    int n_inliers = countInliers(points, analytic_plane);
+
+    return {analytic_plane, n_inliers};
+}
+
+FitResult LeastSquaresFitter::fit(Eigen::MatrixXd const &points)
+{
+    // 1. select `n_sample_points_` points from `points` randomly
+    // 2. compute the equation of the plane using least squares
+    // 3. compute the `n_inliers` given that plane equation
+    Eigen::MatrixXd random_points = getRandomPoints(points, n_sample_points_);
+
+    Eigen::MatrixXd A(n_sample_points_, 3);
+    Eigen::VectorXd b = Eigen::VectorXd::Zero(n_sample_points_);
+
+    // refactoring problem to solve z = a * x + b * y  + d. Minimizing the vertical error
+
+    for (int i = 0; i < n_sample_points_; i++)
+    {
+        A(i, 0) = -random_points(i, 0);
+        A(i, 1) = -random_points(i, 1);
+        A(i, 2) = -1;
+
+        b(i) = random_points(i, 2);
+    }
+
+    Eigen::Vector3d x = (A.transpose() * A).inverse() * A.transpose() * b;
+
+    Plane least_squares_plane;
+    least_squares_plane.a = x(0);
+    least_squares_plane.b = x(1);
+    least_squares_plane.c = 1;
+    least_squares_plane.d = x(2);
+
+    // Normalizing the plane equation
+    double norm = std::sqrt(least_squares_plane.a * least_squares_plane.a + least_squares_plane.b * least_squares_plane.b + least_squares_plane.c * least_squares_plane.c);
+    normalizePlane(least_squares_plane, norm);
+
+    int n_inliers = countInliers(points, least_squares_plane);
+
+    return {least_squares_plane, n_inliers};
 }
