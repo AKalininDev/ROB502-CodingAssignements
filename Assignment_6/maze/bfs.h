ad97393b6bfcc6d5a78e82c6b2a275ffbdbc3dc3
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-24 21:42:44 -0500

    Completed the declaration of BFS and DFS classes

--- Assignment_6/maze/bfs.h
+++ Assignment_6/maze/bfs.h
@@ -1,29 +1,20 @@
 #pragma once
 #include "maze.h"
-// other includes
-// --- Your code here
-
-
-
-// ---
+#include <queue>
 
 /** \brief Breadth First Search solver */
-class BFS : public TreeSearch {
-   public:
+class BFS : public TreeSearch
+{
+public:
     BFS();
 
     /** \brief Solve the planning problem. Override TreeSearch::solve() */
     bool solve();
 
     /** \brief add node to the tree */
-    bool addNode(const NodePtr& node);
+    bool addNode(const NodePtr &node);
 
-   private:
-    // decide what data structure you need
-    // --- Your code here
-
-
-
-    // ---
+private:
+    std::queue<NodePtr> explorationQueue;
 };
 using BFSPtr = std::shared_ptr<BFS>;
--- Assignment_6/maze/dfs.h
+++ Assignment_6/maze/dfs.h
@@ -1,29 +1,20 @@
 #pragma once
 #include "maze.h"
-// other includes
-// --- Your code here
-
-
-
-// ---
+#include <stack>
 
 /** \brief Depth First Search solver */
-class DFS : public TreeSearch {
-   public:
+class DFS : public TreeSearch
+{
+public:
     DFS();
 
     /** \brief Solve the planning problem. Override TreeSearch::solve() */
     bool solve();
 
     /** \brief add node to the tree */
-    bool addNode(const NodePtr& node);
+    bool addNode(const NodePtr &node);
 
-   private:
-    // decide what data structure you need
-    // --- Your code here
-
-
-
-    // ---
+private:
+    std::stack<NodePtr> explorationStack;
 };
 using DFSPtr = std::shared_ptr<DFS>;
