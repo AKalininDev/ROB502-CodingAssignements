b46fd700691c659c987c548bc7e8b34e5012aec6
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-26 14:32:26 -0500

    Completed pi estimate script for HW#7

--- Assignment_7/numpy_warmup/estimate_pi.py
+++ Assignment_7/numpy_warmup/estimate_pi.py
@@ -1,13 +1,30 @@
 import numpy as np
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def inside_unit_circle(point: Point):
+    return point.x**2 + point.y**2 <= 1
+
+
 def estimate_pi(N):
     pi_estimate = None
-    # --- Your code here 
 
+    N_circle = 0
+    random_X_coordinates = np.random.uniform(-1, 1, N)
+    random_Y_coordinates = np.random.uniform(-1, 1, N)
 
+    for x, y in zip(random_X_coordinates, random_Y_coordinates):
+        point = Point(x, y)
 
-    # ---
+        if inside_unit_circle(point):
+            N_circle += 1
+
+    pi_estimate = 4 * N_circle / N
 
     return pi_estimate
 
@@ -15,4 +32,4 @@ def estimate_pi(N):
 if __name__ == "__main__":
     N = 1000
     approx_pi = estimate_pi(N)
-    print(f'Estimate of pi is {approx_pi}')
+    print(f"Estimate of pi is {approx_pi}")
