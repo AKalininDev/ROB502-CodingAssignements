52c71318d88d6df3037d4f965ac729478ef231d1
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-26 20:47:12 -0500

    Fixed code formatting

--- Assignment_7/block_pick_and_place/block_pick_and_place.py
+++ Assignment_7/block_pick_and_place/block_pick_and_place.py
@@ -9,7 +9,11 @@ import pybullet as p
 import pybullet_data as pd
 
 from block_goal_msg import block_goal_t
-from transform_utils import homogeneous_transform_to_pos_quat, pos_quat_to_homogeneous_transform
+from transform_utils import (
+    homogeneous_transform_to_pos_quat,
+    pos_quat_to_homogeneous_transform,
+)
+
 
 class PandaSim:
 
@@ -21,12 +25,16 @@ class PandaSim:
 
         # Change to Y up and change default camera pose
         p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP, 1)
-        p.resetDebugVisualizerCamera(cameraDistance=1, cameraYaw=60, cameraPitch=-30,
-                                     cameraTargetPosition=np.array([0, 0.3, 0]))
+        p.resetDebugVisualizerCamera(
+            cameraDistance=1,
+            cameraYaw=60,
+            cameraPitch=-30,
+            cameraTargetPosition=np.array([0, 0.3, 0]),
+        )
         time.sleep(1)
 
         # Physics sim parameters
-        self.dt = 1. / 100
+        self.dt = 1.0 / 100
         self.controller_steps = 100
         p.setTimeStep(self.dt)
         p.setGravity(0, -9.8, 0)
@@ -36,44 +44,63 @@ class PandaSim:
         flags = p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES
 
         # load plane
-        self.plane = p.loadURDF("plane.urdf", np.array([0.0, 0.0, 0.0]),
-                                [-0.707107, 0.0, 0.0, 0.707107], flags)
+        self.plane = p.loadURDF(
+            "plane.urdf",
+            np.array([0.0, 0.0, 0.0]),
+            [-0.707107, 0.0, 0.0, 0.707107],
+            flags,
+        )
 
         # Load Panda
-        self.panda = p.loadURDF("assets/franka_panda/panda.urdf",
-                                np.array([0, 0, 0]),
-                                [-0.707107, 0.0, 0.0, 0.707107],
-                                useFixedBase=True, flags=flags)
+        self.panda = p.loadURDF(
+            "assets/franka_panda/panda.urdf",
+            np.array([0, 0, 0]),
+            [-0.707107, 0.0, 0.0, 0.707107],
+            useFixedBase=True,
+            flags=flags,
+        )
 
         # load object
         block_orn = p.getQuaternionFromEuler([0.0, 0.03 * math.pi / 2, 0.0])
-        self.block = p.loadURDF("assets/block.urdf", np.array([0.0, 0.05, -0.5]),
-                                block_orn, flags=flags,
-                                globalScaling=1.5)
+        self.block = p.loadURDF(
+            "assets/block.urdf",
+            np.array([0.0, 0.05, -0.5]),
+            block_orn,
+            flags=flags,
+            globalScaling=1.5,
+        )
 
         # Creates constraint between gripper fingers so they move together when opening/closing
-        c = p.createConstraint(self.panda,
-                               9,
-                               self.panda,
-                               10,
-                               jointType=p.JOINT_GEAR,
-                               jointAxis=[1, 0, 0],
-                               parentFramePosition=[0, 0, 0],
-                               childFramePosition=[0, 0, 0])
+        c = p.createConstraint(
+            self.panda,
+            9,
+            self.panda,
+            10,
+            jointType=p.JOINT_GEAR,
+            jointAxis=[1, 0, 0],
+            parentFramePosition=[0, 0, 0],
+            childFramePosition=[0, 0, 0],
+        )
         p.changeConstraint(c, gearRatio=-1, erp=0.1, maxForce=50)
 
         # Setup panda arm
         self.default_joint_pos = [0.98, 0.1, 0.1, -2.24, -0.30, 2.66, 2.32, 0.02, 0.02]
         index = 0
         for j in range(p.getNumJoints(self.panda)):
-            p.changeDynamics(self.panda, j, linearDamping=10, angularDamping=10, jointLowerLimit=-99,
-                             jointUpperLimit=99)
+            p.changeDynamics(
+                self.panda,
+                j,
+                linearDamping=10,
+                angularDamping=10,
+                jointLowerLimit=-99,
+                jointUpperLimit=99,
+            )
             info = p.getJointInfo(self.panda, j)
             jointType = info[2]
-            if (jointType == p.JOINT_PRISMATIC):
+            if jointType == p.JOINT_PRISMATIC:
                 p.resetJointState(self.panda, j, self.default_joint_pos[index])
                 index = index + 1
-            if (jointType == p.JOINT_REVOLUTE):
+            if jointType == p.JOINT_REVOLUTE:
                 p.resetJointState(self.panda, j, self.default_joint_pos[index])
                 index = index + 1
 
@@ -127,15 +154,16 @@ class PandaSim:
             state: 7 dimensional joint angles
         """
         for i in range(7):
-            p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, state[i],
-                                    force=5 * 240.)
+            p.setJointMotorControl2(
+                self.panda, i, p.POSITION_CONTROL, state[i], force=5 * 240.0
+            )
         self.step()
 
     def get_gripper_pose(self):
         """
         :return:
             pose: 4x4 homogeneous transform
-    
+
         """
         pandaEndEffectorIndex = 11
         pos, orn = p.getLinkState(self.panda, pandaEndEffectorIndex)[:2]
@@ -143,8 +171,8 @@ class PandaSim:
 
     def grasp(self):
         """
-              Grasps an object. Lowers grippers, closes grippers, and then raises grippers
-              Only successful if grippers are located in an appropriate pre-grasp pose.
+        Grasps an object. Lowers grippers, closes grippers, and then raises grippers
+        Only successful if grippers are located in an appropriate pre-grasp pose.
         """
         # we lower the end effector slightly and then grasp
         gripper_pose = self.get_gripper_pose()
@@ -152,9 +180,7 @@ class PandaSim:
         self.set_gripper_pose(gripper_pose)
 
         for i in [9, 10]:
-            p.setJointMotorControl2(self.panda, i,
-                                    p.POSITION_CONTROL, 0.003,
-                                    force=10)
+            p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, 0.003, force=10)
 
         # now we rise up again to
         gripper_pose = self.get_gripper_pose()
@@ -163,7 +189,7 @@ class PandaSim:
 
     def ungrasp(self):
         """
-            Ungrasps an object. Lowers grippers, releases grippers, and then raises grippers
+        Ungrasps an object. Lowers grippers, releases grippers, and then raises grippers
         """
         # we lower the end effector slightly and then grasp
         gripper_pose = self.get_gripper_pose()
@@ -171,9 +197,7 @@ class PandaSim:
         self.set_gripper_pose(gripper_pose)
 
         for i in [9, 10]:
-            p.setJointMotorControl2(self.panda, i,
-                                    p.POSITION_CONTROL, 0.04,
-                                    force=10)
+            p.setJointMotorControl2(self.panda, i, p.POSITION_CONTROL, 0.04, force=10)
 
         # now we rise up again to
         gripper_pose = self.get_gripper_pose()
@@ -206,11 +230,14 @@ class PandaSim:
             p.resetJointState(self.panda, j, joint_state[j], 0)
 
         # Calculate the jacobian
-        linear_jacobian, angular_jacobian = p.calculateJacobian(self.panda, pandaEndEffectorIndex,
-                                                                np.zeros(3).tolist(),
-                                                                joint_state.tolist(),
-                                                                np.zeros(9).tolist(),
-                                                                np.zeros(9).tolist())
+        linear_jacobian, angular_jacobian = p.calculateJacobian(
+            self.panda,
+            pandaEndEffectorIndex,
+            np.zeros(3).tolist(),
+            joint_state.tolist(),
+            np.zeros(9).tolist(),
+            np.zeros(9).tolist(),
+        )
         # Get the gripper pose
         gripper_pose = self.get_gripper_pose()
 
@@ -218,14 +245,17 @@ class PandaSim:
         for j in range(7):
             p.resetJointState(self.panda, j, current_joint_state[j], 0)
 
-        return gripper_pose, np.vstack([np.array(linear_jacobian), np.array(angular_jacobian)])[:, :7]
+        return (
+            gripper_pose,
+            np.vstack([np.array(linear_jacobian), np.array(angular_jacobian)])[:, :7],
+        )
 
     def block_goal_handler(self, channel, data):
         self.lock.acquire()
         msg = block_goal_t.decode(data)
         self.goal_pose = np.array(msg.transform)
         self.lock.release()
-        
+
     def block_goal_thread_fn(self):
         while self.running:
             self.lcm_obj.handle()
@@ -233,10 +263,9 @@ class PandaSim:
 
     def setup_thread(self):
         self.running = True
-        self.lcm_obj.subscribe('BLOCK_GOAL', self.block_goal_handler)
+        self.lcm_obj.subscribe("BLOCK_GOAL", self.block_goal_handler)
         worker = threading.Thread(target=self.block_goal_thread_fn)
         worker.start()
 
     def exit(self):
         self.running = False
-
