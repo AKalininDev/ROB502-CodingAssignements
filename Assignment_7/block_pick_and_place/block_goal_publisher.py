d4282b2692f0c72f9cf56691824794f76e777fd3
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-26 20:49:11 -0500

    Completed goal publisher node for Assignement 7, block_pick_and_place problem

--- Assignment_7/block_pick_and_place/block_goal_publisher.py
+++ Assignment_7/block_pick_and_place/block_goal_publisher.py
@@ -3,27 +3,40 @@ import time
 import numpy as np
 from block_goal_msg import block_goal_t
 
-def read_goal(fname):
-    transformation = None
-    # --- Your code here
 
+def read_goal(fname: str) -> np.ndarray:
 
+    with open(fname, "r") as f:
+        lines = [line.strip() for line in f if line.strip()]
 
-     # ---
+    translation = np.fromstring(lines[0], sep=" ")
+    rotation = np.array([np.fromstring(line, sep=" ") for line in lines[1:4]])
+
+    transform = np.eye(4)
+    transform[:3, 3] = translation
+    transform[:3, :3] = rotation
 
     return transform
 
-def publish_transform(transform):
-    # --- Your code here
 
+def publish_transform(transform: np.ndarray) -> None:
 
+    lc = lcm.LCM()
 
-    # ---
+    # build message
+    msg = block_goal_t()
+    msg.transform = transform.tolist()
+    msg.timestamp = int(time.time())
+
+    lc.publish("BLOCK_GOAL", msg.encode())
+
+    return None
+
 
 if __name__ == "__main__":
- 
-    transform = read_goal('inputs1.txt')
-    
+
+    transform = read_goal("inputs1.txt")
+
     while True:
         publish_transform(transform)
         time.sleep(0.1)
